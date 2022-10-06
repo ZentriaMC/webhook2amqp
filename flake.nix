5a8cbf35eaf8323bb51cdc59381b2652ae8ac88c
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-10-06 23:14:03 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "webhook2amqp";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    let
+      # https://rust-lang.github.io/rustup-components-history/
+      rustVersion = "1.64.0";
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rust = pkgs.rust-bin.stable."${rustVersion}".minimal.override {
+          extensions = [ "clippy" "rust-src" "rustfmt" "rust-analyzer" ];
+        };
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            rust
+          ];
+        };
+      });
+}
