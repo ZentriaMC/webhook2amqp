fcc49a23828ee68052f436c0a200c169d5f829dc
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-07-10 23:05:38 +0300

    Build using crane

--- flake.nix
+++ flake.nix
@@ -5,12 +5,14 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
 
+    crane.inputs.nixpkgs.follows = "nixpkgs";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
     rust-overlay.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane, ... }:
     let
       # https://rust-lang.github.io/rustup-components-history/
       rustVersion = "1.69.0";
@@ -31,8 +33,31 @@
         rust = pkgs.rust-bin.stable."${rustVersion}".minimal.override {
           extensions = [ "clippy" "rust-src" "rustfmt" "rust-analyzer" ];
         };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust;
       in
       rec {
+        packages.webhook2amqp = pkgs.callPackage
+          ({ lib, stdenv, pkg-config, luajit, libiconv, Security }: craneLib.buildPackage {
+            src = craneLib.cleanCargoSource (./.);
+
+            buildInputs = [
+              luajit
+            ] ++ lib.optionals stdenv.isDarwin [
+              libiconv
+              Security
+            ];
+
+            nativeBuildInputs = [
+              pkg-config
+            ];
+
+            doCheck = false; # no tests
+          })
+          {
+            inherit (pkgs.darwin.apple_sdk.frameworks) Security;
+          };
+
         devShell = pkgs.mkShell {
           buildInputs = [
             rust
